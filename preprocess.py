5984a2f72aaa0059ee32f0a02617f4262c7c12cb
TheGreek97 <47224664+TheGreek97@users.noreply.github.com>
2022-06-12 20:54:15 +0200

    Enron dataset feature extraction

--- preprocess.py
+++ preprocess.py
@@ -2,27 +2,32 @@ import os
 import pandas
 import feature_extraction as fe
 import json
+import math
+
 
 def enron_dataset():
     base_path = 'datasets/raw/enron/'
     for dataset in ['emails-phishing.csv', 'emails-enron.csv']:
-        df = pandas.read_csv(os.path.join(base_path, dataset), encoding='utf-8', encoding_errors='ignore')
+        df = pandas.read_csv(os.path.join(base_path, dataset), encoding='utf-8')
         # print(df.columns)
         df = df.reset_index()  # make sure indexes pair with number of rows
-        print (df.iloc[251])
-        """
+        item_class = 1 if (dataset == 'emails-phishing.csv') else 0
+        filename_class = "phishing_" if (item_class == 1) else "legit_"
+        print("DATASET: ", dataset)
         for index, row in df.iterrows():
             mail = row['Message']
-            features = fe.extract_features(mail)
-            features["class"] = 1  # PHISHING
-            feature_path = 'datasets\\features'
-            try:
-                with open(os.path.join(feature_path, "phishing_" + str(index)) + ".json", 'x') as output:
-                    output.write(json.dumps(features))
-            except FileExistsError:
-                with open(os.path.join(feature_path, "phishing_" + str(index)) + ".json", 'w') as output:
-                    output.write(json.dumps(features))"""
-
+            if not pandas.isnull(mail) and mail != "":
+                filename = filename_class + str(index) + ".json"
+                print(filename)
+                features = fe.extract_features(mail)
+                features["class"] = item_class  # PHISHING
+                feature_path = 'datasets\\features'
+                try:
+                    with open(os.path.join(feature_path, filename), 'x') as output:
+                        output.write(json.dumps(features))
+                except FileExistsError:
+                    with open(os.path.join(feature_path, filename), 'w') as output:
+                        output.write(json.dumps(features))
         """with open(os.path.join(base_path, dataset), mode='r', encoding='utf-8', errors='ignore') as csv_file:
             mails = csv.reader(csv_file)
             for mail in mails:
@@ -42,7 +47,7 @@ def spam_assassin_dataset():
         with open(file_path, mode='r') as m:
             mail = m.read()
             # TODO separate mails within a single file (divided by -------------------------)
-            features = extract_features(mail)
+            features = fe.extract_features(mail)
             feature_path = 'datasets/features'
             # with open(os.path.join(feature_path, m), 'w') as output:
             #    output.write(features)
