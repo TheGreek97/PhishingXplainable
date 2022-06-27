c6c435dfedc40bc6a5616e187cd005618ca5f816
TheGreek97 <47224664+TheGreek97@users.noreply.github.com>
2022-06-27 16:10:30 +0200

    Global feature importance computed for LIME

--- explanation.py
+++ explanation.py
@@ -4,10 +4,11 @@ from matplotlib import pyplot
 from heapq import nsmallest
 from data import load_data
 import warnings
-
+import shap
 import numpy as np
 import os
 import pickle
+import json
 
 
 def load_model(file_name):
@@ -126,19 +127,40 @@ def tree_local_explanation(clf, x_test, start_index=0, end_index=10, n_top_featu
 
 def lime_explain(model, lime_explainer, x_test, y_test, file_name='lime', start_index=0, end_index=10, show=True, save_file=True):
     warnings.filterwarnings(action='ignore', category=UserWarning)
-
+    explanations = []
+    predictions = []
     for i in range(start_index, end_index):
         instance = x_test.iloc[i]
         real_class = 'Legit (0)' if y_test.iloc[i].item() == 0 else 'Phishing (1)'
         print("Instance " + str(i) + f" - Real class: {real_class}")
         folder_name = str(i)+"_"+str(y_test.iloc[i].item())
         # DECISION TREE
-        explanation_model, predictions = explain(lime_explainer, instance, model, len(feature_names))
+        explanation_model, prediction = explain(lime_explainer, instance, model, len(feature_names))
         if show:
             show_explanation(explanation_model, "Explanation " + file_name)
         if save_file:
             save_explanation_to_file(explanation_model, file_name, folder_name=folder_name)
-        return explanation_model, predictions
+        explanations.append(explanation_model.as_list())
+        predictions.append(prediction)
+    return explanations, predictions
+
+
+# Calcola percentuale di quanto ciascuna feature viene tenuta in conto durante le explanation di LIME e SHAP
+# (e anche quello dell'albero). Il miglior modello è quello con più eterogeneità nelle features
+def calculate_global_feature_importance(explanations, feature_names, file_name):
+    feature_presence = {f: 0 for f in feature_names}
+    for e in explanations:
+        for i in range(0, 3):  # take the top 3 features
+            top_feature = e[i][0].split(' ')[0]
+            if top_feature[0].isdigit():
+                top_feature = e[i][0].split(' ')[2]
+            feature_presence[top_feature] += 1  # increase the feature by one if it is in the top 3 features
+    base_path = os.path.join('output', 'feature_importance')
+    file_path = os.path.join(base_path, file_name+'.json')
+
+    mode = 'w' if os.path.exists(file_path) else 'x'
+    with open(file_path, mode) as out:
+        out.write(json.dumps(feature_presence))
 
 
 if __name__ == "__main__":
@@ -153,16 +175,16 @@ if __name__ == "__main__":
     lr_model = load_model('logistic_regression.obj')
 
     start_test = 0
-    end_test = 10
+    end_test = len(y_test)
 
     # tree_global_explanation(dt_model)
-    tree_local_explanation(dt_model, x_test, start_test, end_test)
+    # tree_local_explanation(dt_model, x_test, start_test, end_test)
     # LIME Explanations for single instances of the test set
     lime_explainer = lime_tabular.LimeTabularExplainer(x_training.values, mode="classification",
                                                        class_names=['Legit', 'Phishing'],
                                                        feature_names=feature_names, random_state=seed)
 
-    lime_explain(dt_model, lime_explainer, x_test, y_test, 'decision_tree', start_test, end_test)
+    # lime_explain(dt_model, lime_explainer, x_test, y_test, 'decision_tree', start_test, end_test)
     # lime_explain(svm_model, lime_explainer, x_test, y_test, 'svm', start_test, end_test)
     # lime_explain(rf_model, lime_explainer, x_test, y_test, 'random_forest', start_test, end_test)
     # lime_explain(lr_model, lime_explainer, x_test, y_test, 'logistic_regression', start_test, end_test)
@@ -173,3 +195,20 @@ if __name__ == "__main__":
     #                                                                    features_to_remove=3)
     # save_explanation_to_file(explanation_rf_1, 'random_forest_1')
     # print('Difference:', predictions_instance[0] - predictions_instance_1[0])
+
+    # ----- SHAP -----
+    X100 = shap.utils.sample(x_test, 100)
+    explainer = shap.Explainer(svm_model.predict, X100)
+
+    # Global feature importance
+    explanations_svm, _ = lime_explain(svm_model, lime_explainer, x_test, y_test, 'svm', start_test, end_test, show=False,
+                                       save_file=False)
+    calculate_global_feature_importance(explanations_svm, feature_names, 'svm')
+
+    explanations_rf, _ = lime_explain(rf_model, lime_explainer, x_test, y_test, 'rf', start_test, end_test, show=False,
+                                      save_file=False)
+    calculate_global_feature_importance(explanations_rf, feature_names, 'rf')
+
+    explanations_lr, _ = lime_explain(lr_model, lime_explainer, x_test, y_test, 'lr', start_test, end_test, show=False,
+                                      save_file=False)
+    calculate_global_feature_importance(explanations_lr, feature_names, 'lr')
