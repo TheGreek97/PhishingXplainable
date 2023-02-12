973fbf2c97bdf1074f427fef9d29ae0a9a9d8070
TheGreek97 <47224664+TheGreek97@users.noreply.github.com>
2023-02-12 19:02:47 +0100

    ITASEC package

--- nn.py
+++ nn.py
@@ -15,9 +15,7 @@ from sklearn.preprocessing import MinMaxScaler
 def custom_loss():
     # Create a loss function that adds the MSE loss to the mean of all squared activations of a specific layer
     def loss(y_true, y_pred):
-        return K.binary_crossentropy(y_true, y_pred)  # TODO + h_score_loss()
-
-    # Return a function
+        return K.binary_crossentropy(y_true, y_pred)
     return loss
 
 
@@ -45,22 +43,22 @@ def mlp_model_builder(hp):
     # Layer 1
     model.add(
         layers.Dense(
-            units=hp.Int('units', min_value=16, max_value=256, step=16),
+            units=hp.Int('units', min_value=16, max_value=64, step=16),
             activation='relu',
             name='dense_1')
     )
     # Layer 2
     model.add(
-        layers.Dense(units=hp.Int('units', min_value=16, max_value=256, step=16),
+        layers.Dense(units=hp.Int('units', min_value=16, max_value=64, step=16),
                      activation='relu',
                      name='dense_2')
     )
     # Output layer
     model.add(layers.Dense(2, activation="softmax", name='output'))
     # Tune the learning rate for the optimizer (0.01, 0.001, or 0.0001)
-    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3])
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
-                  loss=keras.losses.SparseCategoricalCrossentropy(),
+                  loss=keras.losses.BinaryCrossentropy(),
                   metrics=['accuracy', 'recall'])
     return model
 
@@ -75,27 +73,31 @@ def deep_model_builder(hp):
     model.add(keras.layers.Input(shape=(18,)))
     model.add(layers.Normalization(axis=None))
     # Tune the number of units in the first Dense layer
-    # Choose an optimal value between 16-256
-    hp_units = hp.Int('units', min_value=16, max_value=256, step=16)
-    model.add(keras.layers.Dense(units=hp_units, activation='relu', name='dense_1'))
 
-    hp_units = hp.Int('units', min_value=16, max_value=512, step=16)
-    model.add(keras.layers.Dense(units=hp_units, activation='relu', name='dense_2'))
+    model.add(keras.layers.Dense(units=hp.Int('units', min_value=16, max_value=64, step=16),
+                                 activation='relu',
+                                 name='dense_1'))
 
-    hp_units = hp.Int('units', min_value=16, max_value=512, step=16)
-    model.add(keras.layers.Dense(units=hp_units, activation='relu', name='dense_3'))
+    model.add(keras.layers.Dense(units=hp.Int('units', min_value=32, max_value=128, step=16),
+                                 activation='relu',
+                                 name='dense_2'))
 
-    hp_units = hp.Int('units', min_value=16, max_value=256, step=16)
-    model.add(keras.layers.Dense(units=hp_units, activation='relu', name='dense_4'))
+    model.add(keras.layers.Dense(units=hp.Int('units', min_value=32, max_value=128, step=16),
+                                 activation='relu',
+                                 name='dense_3'))
+
+    model.add(keras.layers.Dense(units=hp.Int('units', min_value=16, max_value=64, step=16),
+                                 activation='relu',
+                                 name='dense_4'))
     # Add next layers
     model.add(keras.layers.Dropout(0.2))
 
     model.add(keras.layers.Dense(2, activation="softmax", name='output'))
     # Tune the learning rate for the optimizer
     # Choose an optimal value from 0.01, 0.001, or 0.0001
-    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3])
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
-                  loss=keras.losses.SparseCategoricalCrossentropy(),
+                  loss=keras.losses.BinaryCrossentropy(),
                   metrics=['accuracy', 'recall'])
     return model
 
@@ -105,7 +107,7 @@ def fit_model(model, X, y, class_weight):
         # min_delta: Minimum change in the monitored quantity to qualify as an improvement
         # patience: Number of epochs with no improvement after which training will be stopped
         # restore_best_weights: Whether to restore model weights from the epoch with the best value of val_loss
-        callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=10, restore_best_weights=True)
+        callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=10)
     ]
     model.compile(loss=keras.losses.BinaryCrossentropy(), metrics=[keras.metrics.BinaryAccuracy(),
                                                                    keras.metrics.Recall()])
@@ -119,27 +121,30 @@ def get_optimal_net(X, y, n_fold=5, seed=0, deep=False, verbose=0):
     cv = StratifiedKFold(n_splits=n_fold, shuffle=True, random_state=seed)
     best_model = None
     best_score = 1
-    for ix_train, ix_val in cv.split(X, y):
+    split_ixs = cv.split(X, y)
+    i = 0
+    for ix_train, ix_val in split_ixs:
         # Get data in fold
         x_train, x_val = format_x_data(X.iloc[ix_train]), format_x_data(X.iloc[ix_val])
         y_train, y_val = format_y_data(y.iloc[ix_train]), format_y_data(y.iloc[ix_val])
-
+        folder_name = "dnn_tuning" if deep else "mlp_tuning"
+        folder_name += "_" + str(i)
+        i += 1
         # Prepare tuner
         model_builder = deep_model_builder if deep else mlp_model_builder
         tuner = kt.RandomSearch(model_builder,  # the hyper-model
                                 objective=kt.Objective('val_loss', 'min'),  # objective to optimize
                                 max_trials=10,
                                 executions_per_trial=5,
-                                overwrite=True,
                                 directory='logs',  # directory to save logs
-                                project_name='xai_phishing',
+                                project_name=folder_name,
                                 seed=seed,
                                 loss=keras.losses.BinaryCrossentropy())
         # hyper-tuning settings
         tuner.search_space_summary()
 
-        stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
-        tuner.search(x_train, y_train, epochs=100, validation_data=(x_val, y_val), verbose=verbose, callbacks=[stop_early])
+        # stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+        tuner.search(x_train, y_train, epochs=100, validation_data=(x_val, y_val), verbose=verbose)
 
         h_model = tuner.get_best_models(num_models=1)[0]
         h_model.build()
--- training.py
+++ training.py
@@ -15,9 +15,10 @@ from sklearn.metrics import classification_report
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV, ParameterGrid, StratifiedKFold
+from interpret.glassbox import ExplainableBoostingClassifier
 
 from sklearn.inspection import permutation_importance
-from sklearn.metrics import f1_score, roc_curve, roc_auc_score, precision_recall_curve, auc, make_scorer, recall_score, accuracy_score, precision_score
+from sklearn.metrics import f1_score, roc_auc_score, make_scorer
 
 import winsound
 import tensorflow as tf
@@ -59,11 +60,6 @@ def computeBestModelConfig(X, y, model, param_space, n_fold=5, seed=0, verbose=2
             temp_model = clone(model).set_params(**params)
             fitted_model = temp_model.fit(X.iloc[ix_train], y.iloc[ix_train].values.ravel())
             X_val, y_val = X.iloc[ix_val], y.iloc[ix_val].values.ravel()
-            # get permutation importance (https://scikit-learn.org/stable/modules/permutation_importance.html)
-            #importance = permutation_importance(fitted_model, X_val, y_val, scoring='neg_mean_squared_error').importances_mean
-            #scorer = make_scorer(custom_score, greater_is_better=True, feature_importance=importance,
-            #                     verbose=verbose > 2, needs_proba=False)
-            #score = scorer(fitted_model, X_val, y_val)
             scorer = make_scorer(f1_score)
             score = scorer(fitted_model, X_val, y_val)
             inner_scores.append(score)
@@ -79,53 +75,19 @@ def computeBestModelConfig(X, y, model, param_space, n_fold=5, seed=0, verbose=2
         print(f"Best configuration = {best_params}")
     return best_params
 
-"""
-def computeBestModelConfigNested(X, y, model, param_space, split_ixs, n_fold_inner=5, seed=0, verbose=2):
-    """"""
-        Classifier Hyper-parameter tuning within a nested CV
-    """"""
-    outer_results = list()
-    outer_loop = 0
-    best_params = {}
 
-    # split_ixs = list(split_ixs)[0:1]  # take only the first outer split
-    for train_ix, test_ix in split_ixs:  # Outer CV
-        outer_loop += 1
-        if verbose > 0:
-            print(f"Outer Fold: {outer_loop}")
-        # split data
-        X_train, X_test = X.iloc[train_ix, :], X.iloc[test_ix, :]
-        y_train, y_test = y.iloc[train_ix].values.ravel(), y.iloc[test_ix].values.ravel()
-        best_params = computeBestModelConfig(X_train, y_train, model, param_space, n_fold_inner, seed, verbose)
-
-        # evaluate model on the hold out dataset
-        best_model = clone(model).set_params(**best_params)
-        best_model.fit(X_train, y_train)
-        y_predicted = best_model.predict(X_test)
-        f1 = f1_score(y_test, y_predicted)
-        outer_results.append(f1)  # store the results
-        # report progress
-        if verbose > 0:
-            displayConfusionMatrix(y_test, y_predicted)
-            print(f'{outer_loop}> f1-score=%.3f - {best_params}' % f1)
-    # summarize the estimated performance of the model
-    if verbose > 0:
-        print(f'Mean F1-Score: (mean = {np.mean(outer_results)}, std = {np.std(outer_results)})')
-        print(f'Best configuration: {best_params})')
-    return best_params
-"""
-
-
-def test_model_cv(model, X, y, split_ixs, print_=False, name='Results'):
+def test_model_cv(model, X, y, cv, print_=False, name='Results'):
     metrics = []
+    split_ixs = cv.split(X, y)
     for train_ix, test_ix in split_ixs:
         x_train, y_train = X.iloc[train_ix], y.iloc[train_ix].values.ravel()
         x_test, y_test = X.iloc[test_ix], y.iloc[test_ix].values.ravel()
 
         model.fit(x_train, y_train)
         predictions = model.predict(x_test)
-        metrics.append(
-            classification_report(y_test, predictions, target_names=['Legit', 'Phishing'], output_dict=True))
+        results = classification_report(y_test, predictions, target_names=['Legit', 'Phishing'], output_dict=True)
+        results["roc_auc_score"] = roc_auc_score(y_test, model.predict_proba(x_test)[:, 1])
+        metrics.append(results)
         if print_:
             displayConfusionMatrix(y_test, predictions, name)
             print(name+": ", classification_report(y_test, predictions, target_names=['Legit', 'Phishing']))
@@ -151,6 +113,7 @@ def get_avg_metrics(metrics):
     recalls = []
     npvs = []
     specificities = []
+    auc_scores = []
     n = len(metrics)
     n = n if n > 0 else 1
     for result in metrics:
@@ -160,6 +123,8 @@ def get_avg_metrics(metrics):
         recalls.append(result["Phishing"]["recall"])
         npvs.append(result["Legit"]["precision"])
         specificities.append(result["Legit"]["recall"])
+        if "roc_auc_score" in result:
+            auc_scores.append(result["roc_auc_score"])
     return {
         "accuracy": sum(accuracies) / n,
         "f1_score": sum(f1_scores) / n,
@@ -167,6 +132,7 @@ def get_avg_metrics(metrics):
         "recall": sum(recalls) / n,
         "npv": sum(npvs) / n,
         "specificity": sum(specificities) / n,
+        "roc_auc_score": sum(auc_scores)/n
     }
 
 
@@ -175,23 +141,27 @@ def saveModel(model, file_name):
         pickle.dump(model, write_file)
 
 
-def test_nn_cv(nn_model, X, y, split_ixs, class_weight, print_=False, name='Results'):
+def test_nn_cv(nn_model, X, y, split_ixs, class_weight=None, print_=False, name='Results'):
     metrics = []
     for train_ix, test_ix in split_ixs:
         x_train_nn, x_test_nn = X[train_ix, :], X[test_ix, :]
         y_train_nn, y_test_nn = y[train_ix], y[test_ix]
         model = keras.models.clone_model(nn_model)
+        if class_weight is None:
+            class_weight = {0: 1, 1: 1}
         model = nn.fit_model(model, x_train_nn, y_train_nn, class_weight)
         # model.fit(x=x_train_nn, y=y_train_nn, epochs=200, verbose=2, callbacks=callbacks_list,
         #          shuffle=True, validation_split=0.0, class_weight=class_weight)
         predictions = model.predict(x_test_nn, verbose=1, use_multiprocessing=True, workers=12)
-        predictions = np.argmax(predictions, axis=1)
+        predictions_labels = np.argmax(predictions, axis=1)
         y_test_nn = np.argmax(y_test_nn, axis=1)
-        metrics.append(classification_report(y_test_nn, predictions, target_names=['Legit', 'Phishing'],
-                                             output_dict=True))
-        if (print_):
-            displayConfusionMatrix(y_test_nn, predictions, name)
-            print(f"{name}: ", classification_report(y_test_nn, predictions, target_names=['Legit', 'Phishing']))
+        results = classification_report(y_test_nn, predictions_labels, target_names=['Legit', 'Phishing'], output_dict=True)
+        results["roc_auc_score"] = roc_auc_score(y_test_nn, predictions[:, 1])
+        metrics.append(results)
+        if print_:
+            displayConfusionMatrix(y_test_nn, predictions_labels, name)
+            print(f"{name}: ", classification_report(y_test_nn, predictions_labels, target_names=['Legit', 'Phishing']))
+    metrics = get_avg_metrics(metrics)
     return metrics
 
 
@@ -200,13 +170,13 @@ if __name__ == '__main__':
     np.random.seed(seed)  # set random state also for sklearn
     tf.random.set_seed(seed)
 
-    execute_decision_tree = True
+    execute_decision_tree = False
     execute_logistic_regression = False
     execute_svm = False
     execute_random_forest = False
     execute_ebm = False
     execute_mlp = True
-    execute_dnn = False
+    execute_dnn = True
 
     # LOAD THE DATA
     X, y, feature_names = load_data_no_split()
@@ -214,7 +184,6 @@ if __name__ == '__main__':
 
     n_folds = 5
     cv_outer = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=seed)
-    test_cv_ixs = cv_outer.split(X, y)
 
     # --- DECISION TREE ----
     if execute_decision_tree:
@@ -225,12 +194,12 @@ if __name__ == '__main__':
             'min_samples_leaf': [7],
             'max_depth': [8]
         }
-        best_params_dt = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
-        # best_params_dt = {'ccp_alpha': 0.0, 'criterion': 'entropy', 'max_depth': 8, 'min_samples_leaf': 7}
+        # best_params_dt = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
+        best_params_dt = {'ccp_alpha': 0.0, 'criterion': 'entropy', 'max_depth': 8, 'min_samples_leaf': 7}
         dt_model = model.set_params(**best_params_dt)
         # Evaluate
-        metrics_dt = test_model_cv(dt_model, X, y, test_cv_ixs, print_=True, name='DT')
-        print(metrics_dt)
+        metrics_dt = test_model_cv(dt_model, X, y, cv_outer, print_=False, name='DT')
+        print("DT:", metrics_dt)
         dt_model.fit(X, y)
         saveModel(dt_model, 'decision_tree')
         # showTree(dt_model, feature_names)
@@ -239,17 +208,17 @@ if __name__ == '__main__':
     if execute_logistic_regression:
         # print(best_parameters_lr)
         model = LogisticRegression(random_state=seed)
-        # best_parameters_lr = {'solver': 'lbfgs', 'c': 10, 'penalty': 'l2', 'class_weights': {0: 1, 1: 2}}
         space = {
             'solver': ['newton-cg', 'lbfgs', 'liblinear'],
             'C': [100, 10, 1.0, 0.1, 0.01],
             'penalty': ['l2']
         }
-        best_params_lr = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
+        # best_params_lr = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
+        best_params_lr = {'C': 100, 'penalty': 'l2', 'solver': 'lbfgs'}
         lr_model = model.set_params(**best_params_lr)
         # Evaluate
-        metrics_lr = test_model_cv(lr_model, X, y, test_cv_ixs, print_=True, name='LR')
-        print(metrics_lr)
+        metrics_lr = test_model_cv(lr_model, X, y, cv_outer, print_=False, name='LR')
+        print("LR:", metrics_lr)
         lr_model.fit(X, y)  # Fit the final model on the whole dataset
         saveModel(lr_model, 'logistic_regression')
 
@@ -262,15 +231,15 @@ if __name__ == '__main__':
             'degree': [3, 5, 7],
             'kernel': ['poly']
         }
-        best_params_svm = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
-        # best_parameters_svm = {'C': 10, 'degree': 3, 'gamma': 1, 'kernel': 'poly'}
+        # best_params_svm = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
+        best_params_svm = {'C': 100, 'degree': 3, 'gamma': 0.1, 'kernel': 'poly'}
 
         svm_model = model.set_params(**best_params_svm)
         svm_model.fit(X, y.values.ravel())  # Fit the final model on the whole dataset
         svm_model = make_pipeline(StandardScaler(), svm_model)
 
-        metrics_svm = test_model_cv(svm_model, X, y, test_cv_ixs, print_=True, name='SVM')
-        print(metrics_svm)
+        metrics_svm = test_model_cv(svm_model, X, y, cv_outer, print_=False, name='SVM')
+        print("SVM:", metrics_svm)
 
         saveModel(svm_model, 'svm')
 
@@ -278,24 +247,22 @@ if __name__ == '__main__':
     if execute_random_forest:
         model = RandomForestClassifier(random_state=seed)
         space = {
-            # 'class_weight': [{0: 1, 1: w} for w in [1, 2, 5, 10]],
             'n_estimators': [10, 50, 100],
             'max_features': [5, 10, 14, 18],
             'max_samples': [0.1, 0.25, 0.5]
         }
-        best_params_rf = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
+        # best_params_rf = computeBestModelConfig(X_train, y_train, model=model, param_space=space, seed=seed)
+        best_params_rf = {'max_features': 5, 'max_samples': 0.5, 'n_estimators': 50}
         rf_model = model.set_params(**best_params_rf)
-        metrics_rf = test_model_cv(rf_model, X, y, test_cv_ixs, print_=True, name='RF')
-        print(metrics_rf)
+        metrics_rf = test_model_cv(rf_model, X, y, cv_outer, print_=False, name='RF')
+        print("RF:", metrics_rf)
         rf_model.fit(X, y.values.ravel())  # Fit the final model on the whole dataset
         saveModel(rf_model, 'random_forest')
 
     # ---- EBM -----
     if execute_ebm:
-        # ebm_model = ebm.train(x_train=X_train, y_train=y_train, feature_names=feature_names, class_weight=1, seed=seed)
         metrics = list()
-        from interpret.glassbox import ExplainableBoostingClassifier
-
+        test_cv_ixs = cv_outer.split(X, y)
         for train_ix, test_ix in test_cv_ixs:
             x_train_, y_train_ = X.iloc[train_ix], y.iloc[train_ix]
             x_test_, y_test_ = X.iloc[test_ix], y.iloc[test_ix]
@@ -306,12 +273,13 @@ if __name__ == '__main__':
             ebm_model = ebm.train(x_train=X_train, y_train=y_train, feature_names=feature_names, class_weight=1,
                                   seed=seed)"""
             predictions = ebm_model.predict(x_test_)
-            metrics.append(
-                classification_report(y_test_, predictions, target_names=['Legit', 'Phishing'], output_dict=True))
-            displayConfusionMatrix(y_test_, predictions, "EBM")
-            print("EBM: ", classification_report(y_test_, predictions, target_names=['Legit', 'Phishing']))
+            results_ebm = classification_report(y_test_, predictions, target_names=['Legit', 'Phishing'], output_dict=True)
+            results_ebm["roc_auc_score"] = roc_auc_score(y_test_, ebm_model.predict_proba(x_test_)[:, 1])
+            metrics.append(results_ebm)
+            # displayConfusionMatrix(y_test_, predictions, "EBM")
+            # print(classification_report(y_test_, predictions, target_names=['Legit', 'Phishing']))
         metrics_ebm = get_avg_metrics(metrics)
-        print(metrics_ebm)
+        print("EBM:", metrics_ebm)
         # Fit the final model on the whole dataset
         # ebm_model = ebm.train(x_train=X, y_train=y, feature_names=feature_names, seed=seed)
         # saveModel(ebm_model, 'ebm')
@@ -321,18 +289,24 @@ if __name__ == '__main__':
     X_nn, y_nn = nn.format_x_data(X), nn.format_y_data(y)
     # --- Multi-Layer Perceptron ----
     if execute_mlp:
-        mlp_model = nn.get_optimal_net(X_train, y_train, n_folds, deep=False)
+        mlp_model = nn.get_optimal_net(X_train, y_train, n_folds, seed=42, deep=False)
         class_weights_mlp = {0: 1, 1: 2}
-        test_nn_cv(mlp_model, X_nn, y_nn, test_cv_ixs, print_=True, class_weight=class_weights_mlp, name='MLP')
+        test_cv_ixs = cv_outer.split(X, y)
+        metrics_mlp = test_nn_cv(mlp_model, X_nn, y_nn, test_cv_ixs,  print_=True, name='MLP')
+        print("DNN:", mlp_model.summary())
+        print("Metrics:", metrics_mlp)
         # Fit to the whole dataset
         mlp_model = nn.fit_model(mlp_model, X_nn, y_nn, class_weights_mlp)
         mlp_model.save(os.path.join("models", "mlp"))
 
     # --- Deep Neural Network ----
     if execute_dnn:
-        dnn_model = nn.get_optimal_net(X_train, y_train, n_folds, deep=True)
+        dnn_model = nn.get_optimal_net(X_train, y_train, n_folds, seed=42, deep=True)
         class_weights_dnn = {0: 1, 1: 2}
-        test_nn_cv(dnn_model, X_nn, y_nn, test_cv_ixs, print_=True, class_weight=class_weights_dnn, name='DNN')
+        test_cv_ixs = cv_outer.split(X, y)
+        metrics_dnn = test_nn_cv(dnn_model, X_nn, y_nn, test_cv_ixs, class_weight=class_weights_dnn, print_=True, name='DNN')
+        print("DNN:", dnn_model.summary())
+        print("Metrics:", metrics_dnn)
         # Fit to the whole dataset
         dnn_model = nn.fit_model(dnn_model, X_nn, y_nn, class_weights_dnn)
         dnn_model.save(os.path.join("models", "dnn"))
